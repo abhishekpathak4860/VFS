ccb459ae05d5927cb24e5ea00a91a71ccf441455
Abhishek Pathak <abhishekpathak37733@gmail.com>
2025-11-21 03:00:07 +0530

    updated root file

--- Server/app2.js
+++ Server/app2.js
@@ -3,10 +3,20 @@ import cors from "cors";
 import fileRoute from "./Routes/fileRoutes.js";
 import folderRoute from "./Routes/directoryRoutes.js";
 
+const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
 const app = express();
 app.use(express.json());
 //enable cors
-app.use(cors());
+app.get("/", (req, res) => {
+  res.json("server is running");
+});
+app.use(
+  cors({
+    origin: [FRONTEND_URL],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    credentials: true,
+  })
+);
 //serving directory
 app.use("/directory", folderRoute);
 //upload folder
@@ -14,6 +24,7 @@ app.use("/folder/upload", folderRoute);
 
 app.use("/file", fileRoute);
 
-app.listen(5000, () => {
-  console.log("Server started successfully");
-});
+// app.listen(5000, () => {
+//   console.log("Server started successfully");
+// });
+export default app;
