a7b821ccb4490402272489c10cfeaebf0a55634d
Abhishek Pathak <abhishekpathak37733@gmail.com>
2026-08-11 00:38:56 +0530

    added atlas db

--- Server/config/db.js
+++ Server/config/db.js
@@ -1,33 +1,13 @@
-// // mongo db connection logic
-// import { MongoClient } from "mongodb";
-// // aws instance ip address 13.232.231.72
-// export const client = new MongoClient(
-//   "mongodb://abhi:abhi@localhost:27017/storageApp?replicaSet=myReplicaSet",
-// );
-
-// export async function connectDB() {
-//   await client.connect();
-//   const db = client.db();
-//   await db.collection("directories").createIndex({ parentDir: 1 });
-//   await db.collection("files").createIndex({ parentDir: 1 });
-//   return db;
-// }
-
-// process.on("SIGINT", async () => {
-//   await client.close();
-//   console.log("db disconnected successfully");
-//   process.exit(0);
-// });
-
 // config/db.js
-
 import mongoose from "mongoose";
 
 export async function connectDB() {
   try {
-    await mongoose.connect(
-      "mongodb://abhi:abhi@localhost:27017/storageApp?replicaSet=myReplicaSet",
-    );
+    const connUri =
+      process.env.MONGO_URI ||
+      "mongodb://abhi:abhi@localhost:27017/storageApp?replicaSet=myReplicaSet";
+
+    await mongoose.connect(connUri);
 
     console.log("MongoDB Connected Successfully");
   } catch (err) {
